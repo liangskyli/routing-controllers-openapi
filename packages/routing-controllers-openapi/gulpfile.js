b9c21a7d7883219130ba8752557fc5e78893e544
liangsky <640634387@qq.com>
2022-08-06 13:13:30 +0800

    chore: upgrade dependencies, node version and prettier setting

--- packages/routing-controllers-openapi/gulpfile.js
+++ packages/routing-controllers-openapi/gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const ts = require('gulp-typescript');
-const del = require('del');
+const del = require('./esm-to-commjs').del;
 const through = require('through2');
 const tsconfig = require('../tsconfig.base.json');
 
 function clean() {
-  return del('./lib/**');
+  return del.deleteAsync('./lib/**');
 }
 
 function buildCJS() {
@@ -28,7 +28,10 @@ function buildES() {
     ...tsconfig.compilerOptions,
     module: 'ESNext',
   });
-  return gulp.src(['src/**/*.{ts,tsx}']).pipe(tsProject).pipe(gulp.dest('lib/es/'));
+  return gulp
+    .src(['src/**/*.{ts,tsx}'])
+    .pipe(tsProject)
+    .pipe(gulp.dest('lib/es/'));
 }
 
 function buildDeclaration() {
@@ -46,7 +49,9 @@ function buildDeclaration() {
 }
 
 function copyMetaFiles() {
-  return gulp.src(['./README.md', 'LICENSE', './bin*/**']).pipe(gulp.dest('./lib/'));
+  return gulp
+    .src(['./README.md', 'LICENSE', './bin*/**'])
+    .pipe(gulp.dest('./lib/'));
 }
 
 function generateCliPackageJSON() {
